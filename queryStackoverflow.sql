5fee4b9fb9428f2e061738de1432e294fb15362f
smeda10 <50984526+smeda10@users.noreply.github.com>
2019-06-03 17:46:39 +0200

    Update queryStackoverflow.sql

--- queryStackoverflow.sql
+++ queryStackoverflow.sql
@@ -1,4 +1,4 @@
-SELECT * FROM posts WHERE (Score + CommentCount >= 8) AND Id < 280000);
+SELECT * FROM posts WHERE (Score + CommentCount >= 8) AND (Id < 280000);
 SELECT * FROM posts WHERE (Score + CommentCount >= 8) AND (Id >= 280000 AND Id < 500000);
 SELECT * FROM posts WHERE (Score + CommentCount >= 8) AND (Id >= 500000 AND Id < 750000);
 SELECT * FROM posts WHERE (Score + CommentCount >= 8) AND (Id >= 750000 AND Id < 1000000);
