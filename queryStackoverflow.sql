62f5b5ed73c8985fa2d66bea9d373d2df007bcf9
smeda10 <50984526+smeda10@users.noreply.github.com>
2019-09-05 20:48:11 +0200

    Delete queryStackoverflow.sql

--- queryStackoverflow.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM posts WHERE (Score + CommentCount >= 8) AND (Id < 280000);
-SELECT * FROM posts WHERE (Score + CommentCount >= 8) AND (Id >= 280000 AND Id < 500000);
-SELECT * FROM posts WHERE (Score + CommentCount >= 8) AND (Id >= 500000 AND Id < 750000);
-SELECT * FROM posts WHERE (Score + CommentCount >= 8) AND (Id >= 750000 AND Id < 1000000);
-
